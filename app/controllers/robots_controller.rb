2d29c3cdb554c9b0204592a6ee3516ede6891e0d
Rene Garcia <rsamuelgarcia@gmail.com>
2026-06-20 22:52:03 -0400

    fix: se arreglaron multiples errores.

--- app/controllers/robots_controller.rb
+++ app/controllers/robots_controller.rb
@@ -11,13 +11,19 @@ class RobotsController < ApplicationController
   def sitemap
     @entries = []
 
-    @entries << SitemapEntry.new(loc: root_url)
-    @entries << SitemapEntry.new(loc: about_url)
-    @entries << SitemapEntry.new(loc: now_url)
-    @entries << SitemapEntry.new(loc: posts_list_url)
+    @entries << SitemapEntry.new(loc: root_url, priority: 1.0, changefreq: "daily")
+    @entries << SitemapEntry.new(loc: posts_list_url, priority: 0.9, changefreq: "daily")
+    @entries << SitemapEntry.new(loc: about_url, priority: 0.5, changefreq: "monthly")
+    @entries << SitemapEntry.new(loc: now_url, priority: 0.6, changefreq: "weekly")
 
     @entries += Post.all.map do |post|
-      SitemapEntry.new(loc: post_url(post), lastmod: post.published_at)
+      SitemapEntry.new(
+        loc: post_url(post),
+        lastmod: post.published_at,
+        priority: 0.8,
+        changefreq: "monthly",
+        image: post.og_image.presence
+      )
     end
   end
 
@@ -29,5 +35,6 @@ class RobotsController < ApplicationController
     attribute :lastmod, :datetime, default: -> { Time.current }
     attribute :changefreq, :string, default: "daily"
     attribute :priority, :float, default: 1.0
+    attribute :image, :string
   end
 end
